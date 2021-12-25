05578dc74ae0aedd29f69476f5c66d7cf43f3b9b
WellWedley <romain.cairol@gmail.com>
2021-12-25 02:38:49 +0100

    tagForCookies call changed

--- pages/footer.php
+++ pages/footer.php
@@ -6,8 +6,23 @@ require_once ('libraries/utils.php');
     /*
      * Fonction pour intégrer les cookies
      */
-include_once 'libraries/utils.php';
-     tagForCookies();
+
+    function tagForCookies(){
+    return    '<script>
+window.axeptioSettings = {
+  clientId: "61b60a0d0c9ed4377d951b63",
+  cookiesVersion: "thau-elagage34-base",
+};
+ 
+(function(d, s) {
+  var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+  e.async = true; e.src = "//static.axept.io/sdk.js";
+  t.parentNode.insertBefore(e, t);
+})(document, "script");
+</script>' ;
+
+    }
+    tagForCookies();
     ?>
     <div class="footer_wrap">
         <!-- Axeptio's cookies scripts -->
