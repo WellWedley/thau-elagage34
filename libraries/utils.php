82a78ec064dcd77b3e4d9f6d4409342b431fecaf
WellWedley <romain.cairol@gmail.com>
2021-12-25 03:14:08 +0100

    this commit is way  to long to solve.

---
+++ libraries/utils.php
@@ -0,0 +1,16 @@
+<?php
+function tagForCookies(){
+   ' <script>
+    window.axeptioSettings = {
+          clientId: "61b60a0d0c9ed4377d951b63",
+  cookiesVersion: "thau-elagage34-base",
+};
+ 
+(function(d, s) {
+      var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+  e.async = true; e.src = "//static.axept.io/sdk.js";
+  t.parentNode.insertBefore(e, t);
+})(document, "script");
+</script>' ;
+
+}
