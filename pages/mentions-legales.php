4ce43d292d31140a38d5ec10495b39839b5c2e09
WellWedley <romain.cairol@gmail.com>
2022-02-26 19:48:13 +0100

    mentions-legales done

--- pages/mentions-legales.php
+++ pages/mentions-legales.php
@@ -19,24 +19,122 @@
         <h1 class="mentions_title">MENTIONS LÉGALES</h1>
         </div>
         <div class="mention-details identification">
-            <h1>IDENTIFICATION </h1>
-            <p>lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum</p>
-
+            <h1>IDENTIFICATION</h1>
+            <p>Le contenu du site Thau Élagage  est disponible à l'adresse :
+                <a class="link site_link"
+                   href="https://thau-elagage34.fr"
+                   target="_blank">https://thau-elagage34.fr</a></p>
         </div>
-        <div class="mention-details owner">
-            <h1>PROPRIÉTAIRE </h1>
-            <p>lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum</p>
 
-        </div>
-        <div class="mention-details creator">
-            <h1>CONCEPTEUR </h1>
-            <p>lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum</p>
+        <div class="mention-details identification">
+            <h1>PROPRIÉTAIRE</h1>
+            <p>Thau Élagage est la propriété de l'entreprise individuelle COLOMBAR Esaie dont voici les coordonées :
 
+            </p>
+
+            <div>
+
+                <h2>Établissement principal :</h2>
+                <p>25 Rue des poiriers, 34540 Balaruc-Les-Bains - France
+                </p>
+
+
+                <h2>
+                    Siège social :
+                </h2>
+                <p>
+                    25 Rue des poiriers, 34540 Balaruc-Les-Bains - France
+                </p>
+
+                <h2>
+                    Numéro de téléphone mobile :
+                </h2>
+                <p>
+                    <a href="tel:+33628350083">+33 06 28 35 00 83</a>
+                </p>
+
+                <h2>
+                   Courrier électronique  :
+                </h2>
+                <p>
+                    <a href="mailto:esa.colo.34@gmail.com ">mail: esa.colo.34@gmail.com </a>
+                </p>
+
+                <h2>
+                  Forme sociale :
+                </h2>
+                <p>
+                    EI ( Entreprise individuelle )
+                </p>
+                <h2>
+                 SIREN :
+                </h2>
+                <p>
+                    833 330 632
+                </p>
+
+            </div>
         </div>
+
+
         <div class="mention-details hosting">
-            <h1>HÉBERGEMENT </h1>
-            <p>lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum</p>
+            <h1>HÉBERGEMENT</h1>
+            <div>
+                <p>Ce site est hébergé par la société PLANETHOSTER
+                </p>
 
+                <h2>
+                    Raison sociale :
+                </h2>
+                <p>
+               PLANET HOSTER
+                </p>
+
+
+                <h2>
+                   Adresse :
+                </h2>
+                <p>
+                    4416 Louis B. Mayer, Laval (Québec) H7P 0G1, Canada
+                </p>
+
+                <h2>
+                    Numéro de téléphone mobile :
+                </h2>
+                <p>
+                    <a href="tel:+1 855-774-4678">+1 855-774-4678</a>
+                </p>
+
+            </div>
+        </div>
+
+        <div class="mention-details hosting">
+            <h1>CONCEPTEUR</h1>
+            <div>
+                <p>Mr CAIROL Romain
+                </p>
+
+                <p>
+                    Conception web
+                </p>
+
+
+                <h2>
+                    Téléphone :
+                </h2>
+                <p>
+                    Mobile : +33 06 27 62 56 58
+                </p>
+
+                <h2>
+                    Siren :
+                </h2>
+                <p>
+                    897 876 041
+                </p>
+                <p>Vous pouvez exercer votre droit d'accès et de rectification aux informations vous concernant en écrivant à :</p>
+                <p><a href="mailto:webboxfactory.com">@webboxfactory.com</a></p>
+            </div>
         </div>
 
     </div>
