801d1d83a43297c6c941be689a20e6e1d0aa4158
WellWedley <romain.cairol@gmail.com>
2022-10-18 11:54:13 +0200

    Retrait Axeptio, ads Problem ?

--- pages/footer.php
+++ pages/footer.php
@@ -1,20 +1,6 @@
-
 <footer>
-  <script>
-window.axeptioSettings = {
-  clientId: "61b60a0d0c9ed4377d951b63",
-  cookiesVersion: "thau-elagage34-base",
-};
- 
-(function(d, s) {
-  var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
-  e.async = true; e.src = "//static.axept.io/sdk.js";
-  t.parentNode.insertBefore(e, t);
-})(document, "script");
-</script>
 
     <div class="footer_wrap">
-        <!-- Axeptio's cookies scripts -->
 
         <div class="outro_wrap">
             <div class="grey_tree_wrap">
@@ -87,7 +73,7 @@ window.axeptioSettings = {
                 <div class="mentions_footer_details">
                     <div>
                         <a href="/pages/mentions-legales.php">Mentions légales</a>
-                    </div> 
+                    </div>
                 </div>
             </div>
         </div>
